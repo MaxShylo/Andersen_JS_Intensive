d62001aa8a6e1f2625e1edec98a8b1334ddcd025
Max Shylenko <maxshylenko@gmail.com>
2022-08-14 16:15:17 +0200

    home-work_2 is done

---
+++ homework_2/homework-2.js
@@ -0,0 +1,64 @@
+'use strict'
+
+// Task-1_makeObjectDeepCopy
+
+function makeObjectDeepCopy(obj) {
+  let clonedObj;
+
+  if (obj === null || typeof obj !== 'object') {
+    return obj;
+  }
+
+  if (obj instanceof Object) {
+    clonedObj = {};
+
+    for (const key in obj) {
+      clonedObj[key] = makeObjectDeepCopy(obj[key]);
+    }
+
+    return clonedObj;
+  }
+}
+
+// Task-2_selectFromInterval
+
+function selectFromInterval(inputsArr, x, y) {
+  if (!(inputsArr instanceof Array)) {
+    throw new Error('The input parameter is not an array');
+  }
+
+  if (!inputsArr.every((input) => Number.isFinite(input))) {
+    throw new Error('The input array should contain only correct numbers');
+  }
+
+  if (!Number.isFinite(x) || !Number.isFinite(y)) {
+    throw new Error('Incorrect interval parameters');
+  }
+
+  const startInterval = (x < y) ? x : y;
+  const endInterval = (x > y) ? x : y;
+
+  return inputsArr.filter((number) => (number > startInterval) && (number <= endInterval));
+}
+
+// Task-3_myIterable
+
+const myIterable = { from: 1, to: 4 };
+
+myIterable[Symbol.iterator] = function() {
+  let { from, to } = this;
+
+  if ((!from || !to) || (to < from) || (typeof from !== 'number' || typeof to !== 'number')) {
+    throw new Error('Incorrect input');
+  }
+
+  return {
+    next() {
+      if (from <= to) {
+        return { done: false, value: from++ }
+      } else {
+        return { done: true }
+      }
+    }
+  };
+};

22af09bfb6aa2a1277d7dd2aa88e41f4900dceb0
Maksym Shylenko <maxshylenko@gmail.com>
2022-08-29 20:30:25 +0200

    updated stack

--- homework_5/stack.js
+++ homework_5/stack.js
@@ -2,14 +2,22 @@
 
 // Task-1_class_Stack
 
+class Node {
+  constructor(element) {
+    this.data = element;
+    this.previous = null;
+  }
+}
+
 class Stack {
-  constructor(maxSize = 10) {
-    if (!Number.isFinite(maxSize)) {
+  constructor(inputSize = 10) {
+    if (!Number.isFinite(inputSize) || inputSize <= 0) {
       throw new Error('Invalid input, input should be corret number!!!');
     }
 
-    this.size = maxSize;
-    this.items = [];
+    this.top = null;
+    this.size = 0;
+    this.maxSize = inputSize;
   }
 
   static fromIterable(iterable) {
@@ -17,52 +25,65 @@ class Stack {
       throw new Error('Non-iterable');
     }
 
-    const stackFromIterable = new Stack([...iterable].length);
-    stackFromIterable.items = [...iterable];
+    const arrDataIterable = [...iterable];
+    const stackFromIterable = new Stack(arrDataIterable.length);
+
+    arrDataIterable.forEach((elem) => {
+      stackFromIterable.push(elem);
+    })
 
     return stackFromIterable;
   }
 
   push(element) {
-    const { size, items } = this;
+    const node = new Node(element);
 
-    if (items.length === size) {
+    if (this.size >= this.maxSize) {
       throw new Error('Stack full!!');
     }
 
-    return (items[items.length] = element);
+    node.previous = this.top;
+    this.top = node;
+    this.size++;
+
+    return this.top;
   }
 
   pop() {
-    const { items } = this;
-
-    if (!items.length) {
+    if (!this.size) {
       throw new Error('Stack is empty!!!');
     }
 
-    const removedElement = items[items.length - 1];
-    items.length--;
+    const removedElement = this.top;
+    this.top = this.top.previous;
+    this.size--;
 
-    return removedElement;
+    return removedElement.data;
   }
 
   peek() {
-    const { items } = this;
+    const { size, top } = this;
 
-    return !items.length ? null : items[items.length - 1];
+    return !size ? null : top.data;
   }
 
   isEmpty() {
-    const { items } = this;
-
-    return items.length === 0;
+    return this.size === 0;
   }
 
   toArray() {
-    const { items } = this;
+    const arrData = [];
 
-    return [...items];
+    let current = this.top;
+    let count = this.size;
+
+    while (count > 0) {
+      arrData[arrData.length] = current.data;
+
+      current = current.previous;
+      count--;
+    }
+
+    return arrData;
   }
 }
-
-module.exports = { Stack };
